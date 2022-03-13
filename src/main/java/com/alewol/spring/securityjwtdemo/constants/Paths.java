03dfa90e462600f2a8669f132e4f08248ffd684a
Alexander Wolf <alexander_wolf91@web.de>
2022-03-13 01:38:52 +0100

    added Constants

---
+++ src/main/java/com/alewol/spring/securityjwtdemo/constants/Paths.java
@@ -0,0 +1,10 @@
+package com.alewol.spring.securityjwtdemo.constants;
+
+public final class Paths {
+
+    public static final String ROOT = "api/v1/";
+
+    public static final String USER = "user/";
+    public static final String ROLE = "role/";
+    
+}
