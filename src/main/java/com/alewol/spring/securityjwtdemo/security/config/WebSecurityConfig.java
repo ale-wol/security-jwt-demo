536efa77bbd3c0908535f14f0de1894a11990d54
Alexander Wolf <alexander_wolf91@web.de>
2022-03-14 23:25:35 +0100

    add Custom Authentication Filter

--- src/main/java/com/alewol/spring/securityjwtdemo/security/config/WebSecurityConfig.java
+++ src/main/java/com/alewol/spring/securityjwtdemo/security/config/WebSecurityConfig.java
@@ -1,7 +1,9 @@
 package com.alewol.spring.securityjwtdemo.security.config;
 
     
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
@@ -11,6 +13,11 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import lombok.RequiredArgsConstructor;
+
+import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
+
+import com.alewol.spring.securityjwtdemo.filter.CustomAuthenticationFilter;
+
 
 @Configuration
 @RequiredArgsConstructor
@@ -27,6 +34,10 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     public void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http.sessionManagement().sessionCreationPolicy(STATELESS);
+        http.authorizeRequests().anyRequest().permitAll();
+        http.addFilter(new CustomAuthenticationFilter(authenticationManagerBean()));
     }
 
     @Override
@@ -34,6 +45,12 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         web     
                 .ignoring()
                 .antMatchers("/h2-console/**");
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
     }
 
 }
