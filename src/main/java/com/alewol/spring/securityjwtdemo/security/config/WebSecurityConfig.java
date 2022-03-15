16aa47632e4e28dfb6a54985a1e3488219a4fbf2
Alexander Wolf <alexander_wolf91@web.de>
2022-03-15 22:31:59 +0100

    added JWT Token Generation

--- src/main/java/com/alewol/spring/securityjwtdemo/security/config/WebSecurityConfig.java
+++ src/main/java/com/alewol/spring/securityjwtdemo/security/config/WebSecurityConfig.java
@@ -3,6 +3,7 @@ package com.alewol.spring.securityjwtdemo.security.config;
     
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -15,6 +16,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import lombok.RequiredArgsConstructor;
 
 import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
+import static com.alewol.spring.securityjwtdemo.constants.Paths.ROOT;
 
 import com.alewol.spring.securityjwtdemo.filter.CustomAuthenticationFilter;
 
@@ -34,10 +36,15 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     public void configure(HttpSecurity http) throws Exception {
+        CustomAuthenticationFilter customAuthenticationFilter = new CustomAuthenticationFilter(authenticationManagerBean());
+        customAuthenticationFilter.setFilterProcessesUrl("/" + ROOT + "login");
         http.csrf().disable();
         http.sessionManagement().sessionCreationPolicy(STATELESS);
-        http.authorizeRequests().anyRequest().permitAll();
-        http.addFilter(new CustomAuthenticationFilter(authenticationManagerBean()));
+        http.authorizeRequests().antMatchers("/" + ROOT + "login/**").permitAll();
+        http.authorizeHttpRequests().antMatchers(HttpMethod.GET, "/" + ROOT + "**").hasAnyAuthority("ROLE_USER");
+        http.authorizeHttpRequests().antMatchers(HttpMethod.POST, "/" + ROOT + "user/save/").hasAnyAuthority("ROLE_USER");
+        http.authorizeRequests().anyRequest().authenticated();
+        http.addFilter(customAuthenticationFilter);
     }
 
     @Override
